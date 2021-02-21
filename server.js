4c9c979a66dfc8b4cea7109764cb6dbf54697531
sudhir <kumarkumarsudhir67@gmail.com>
2021-02-21 21:23:35 +0530

    initial author routes

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ if (process.env.NODE_ENV !== 'production') {
   }
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({limit:'10MB',extended:false}));
 const expressLayouts = require("express-ejs-layouts");
 const path = require('path');
 const routes = require('./routes/index');
+const authors = require('./routes/authors');
 const mongoose = require('mongoose');
 
 
@@ -25,6 +27,7 @@ app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/',routes);
+app.use('/authors',authors);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
 
